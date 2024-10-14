17e2b6431c7339db83cb0276001df3bdd2d72326
Cecilia <ncecilia32d@gmail.com>
2024-10-14 08:58:59 +0300

    Added basic form handling, and added typewriter plugin.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-index.js
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "I Am An African",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
